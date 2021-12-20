9177d5dd5885b8116764436be3d6d278050b77ed
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-12-20 11:32:09 +0000

    init commit

---
+++ src/test/java/com/paakofiaidoo/jmixlearn/JmixLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paakofiaidoo.jmixlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmixLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
