e4c8883e334999d4ce6f2c7ea8803b7d81e48954
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-12-30 22:44:48 +0000

    competed quick start

---
+++ src/main/java/com/paakofiaidoo/jmixlearn/TaskService.java
@@ -0,0 +1,29 @@
+package com.paakofiaidoo.jmixlearn;
+
+import com.paakofiaidoo.jmixlearn.entity.User;
+import io.jmix.core.DataManager;
+import io.jmix.core.DataStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private DataManager dataManager;
+
+    public User findLeastBusyUser() {
+        User user = dataManager.loadValues("select u, count(t.id) "+
+                "from User u left outer join Task_ t " +
+                "on u = t.assignee  " +
+                "group by u order by count(t.id)")
+                .properties("user","tasks")
+                .list().stream()
+                .map(e -> e.<User>getValue("user"))
+                .findFirst()
+                .orElseThrow(IllegalStateException::new);
+        return user;
+
+    }
+}
--- src/main/java/com/paakofiaidoo/jmixlearn/screen/task/TaskEdit.java
+++ src/main/java/com/paakofiaidoo/jmixlearn/screen/task/TaskEdit.java
@@ -1,10 +1,19 @@
 package com.paakofiaidoo.jmixlearn.screen.task;
 
+import com.paakofiaidoo.jmixlearn.TaskService;
 import io.jmix.ui.screen.*;
 import com.paakofiaidoo.jmixlearn.entity.Task;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Task_.edit")
 @UiDescriptor("task-edit.xml")
 @EditedEntityContainer("taskDc")
 public class TaskEdit extends StandardEditor<Task> {
+    @Autowired
+    private TaskService taskService;
+
+    @Subscribe
+    public void onInitEntity(InitEntityEvent<Task> event) {
+        event.getEntity().setAssignee(taskService.findLeastBusyUser());
+    }
 }
